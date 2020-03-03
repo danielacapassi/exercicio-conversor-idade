26afcbd03822abc08301f199e56251f3f36dca48
danielacapassi <61717429+danielacapassi@users.noreply.github.com>
2020-03-03 12:34:54 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// Perguntar a idade  
+var age = parseInt (prompt ("Qual é a sua idade?")); 
+// Calcular idade em segundos
+var ageInSeconds = age * 365 * 24 * 60 * 60;
+// Mostrar a idade na console
+console.log (ageInSeconds)
+document.write ("Em seus " +  age  + " anos de idade, já se passaram " + ageInSeconds+ " segundos")
+
+
